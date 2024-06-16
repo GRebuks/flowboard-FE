0acef7503b90f10cddb4f78fa68aaa8261540868
Gundars Rēbuks <rebuks.gundars@gmail.com>
2024-06-16 23:57:37 +0300

    fix: rollback

--- composables/useApiFetch.ts
+++ composables/useApiFetch.ts
@@ -1,14 +1,14 @@
-import type { UseFetchOptions } from '#imports';
-import { ref } from 'vue';
+import type { UseFetchOptions } from '#imports'
+import {ref} from "vue";
 
-export function useApiFetch<T>(path: string, options: UseFetchOptions<T> = {}) {
+export function useApiFetch<T> (path: string, options: UseFetchOptions<T> = {}){
   let headers: any = {
-    accept: 'application/json',
-    referer: 'https://flowboard-be-f933713bceca.herokuapp.com',
-  };
+    accept: "application/json",
+    referer: "https://flowboard-be-f933713bceca.herokuapp.com",
+  }
 
   const token = useCookie('XSRF-TOKEN', {
-    secure: true,
+    secure: true
   });
 
   if (token.value) {
@@ -18,10 +18,9 @@ export function useApiFetch<T>(path: string, options: UseFetchOptions<T> = {}) {
   if (process.server) {
     headers = {
       ...headers,
-      ...useRequestHeaders(['cookie']),
-    };
+      ...useRequestHeaders(["cookie"])
+    }
   }
-
   const fetchOptions = {
     credentials: 'include',
     watch: false,
@@ -32,32 +31,5 @@ export function useApiFetch<T>(path: string, options: UseFetchOptions<T> = {}) {
     },
   };
 
-  const responseHandler = async (response: Response) => {
-    const setCookieHeader = response.headers.get('Set-Cookie');
-    if (setCookieHeader) {
-      // Parse the Set-Cookie header to extract cookie name and value
-      const cookieParts = setCookieHeader.split(';')[0].split('=');
-      const cookieName = cookieParts[0].trim();
-      const cookieValue = cookieParts[1].trim();
-
-      // Set the cookie with document.cookie
-      document.cookie = `${cookieName}=${cookieValue}; path=/; SameSite=Lax`;
-
-      // Optionally update token.value if it matches the cookie name
-      if (token.value && cookieName === 'XSRF-TOKEN') {
-        token.value = cookieValue;
-      }
-    }
-
-    return response;
-  };
-
-  return useFetch('https://flowboard-be-f933713bceca.herokuapp.com' + path, {
-    ...fetchOptions,
-    onDone: responseHandler,
-    onError: (error: Error) => {
-      console.error('Fetch error:', error);
-      // Handle error as needed
-    },
-  });
+  return useFetch("https://flowboard-be-f933713bceca.herokuapp.com" + path, fetchOptions);
 }
